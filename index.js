b0b72fa9e3301dc465c428b5dd4bfa2661a68069
unknown <tanvirhasan.tanshen2019@gmail.com>
2021-07-05 20:28:25 +0600

    database connection

---
+++ index.js
@@ -0,0 +1,34 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+
+
+//import postRoutes from './routes/posts.js';
+//import userRouter from "./routes/user.js";
+import hellow from './routes/hellow.js';
+
+const app = express();
+dotenv.config();
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+//app.use('/posts', postRoutes);
+//app.use("/user", userRouter);
+
+app.use('/', hellow);
+
+
+
+const PORT = 5000;
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
